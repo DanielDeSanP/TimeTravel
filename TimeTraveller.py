8c2aa1459adad35b9a5bf6c9e885d8967ed8db0d
Luis  Daniel De San Pedro <Tzion@Tzion.local>
2016-05-03 14:38:27 -0500

    El programa ya puede desplegar el tiempo

--- TimeTraveller.py
+++ TimeTraveller.py
@@ -1,5 +1,40 @@
+# -*- coding:utf-8 -*-
 import googleAPI
-Origen = raw_input("Origen: ")
-Destino = raw_input("Destino: ")
+import time
+"""
+	Programa diseñado para hacer un registro
+	estadístico de tiempos de traslado de un punto a otro.
+	Usa una API de Google para obtener esos datos.
+	El programa llama a la API por medio de un solicitud
+	HTTPS cada media hora durante las 24 de un día.
+	
 
-googleAPI.
+"""
+entrada = raw_input("Origen: ")
+palabrasSepa = entrada.split(" ")
+Origen = ""
+Destino = ""
+for palabra in palabrasSepa:
+	Origen += palabra
+
+entrada = raw_input("Destino: ")
+palabrasSepa = entrada.split(" ")
+for palabra in palabrasSepa:
+	Destino += palabra
+print (Origen)
+print (Destino)
+
+tiempo = 60 * 30
+
+try:
+	for i in range(1,48):
+		archivo = open("registroTiempos.txt","a")
+		googleAPI.obtenerTiempo(Origen,Destino)
+		archivo.write("a\n")
+		archivo.close()
+		time.sleep(tiempo)
+
+except KeyboardInterrupt:
+	print("Proceso interrumpido")
+except:
+	print("Algo salio mal")
