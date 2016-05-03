03b51217c32ba6a5b0a03feb69f651ab2185eba1
Luis  Daniel De San Pedro <Tzion@Tzion.local>
2016-05-03 14:38:56 -0500

    Ya puede desplegar el tiempo

--- googleAPI.py
+++ googleAPI.py
@@ -1,20 +1,24 @@
+# -*- coding:utf-8 -*-
 import urllib, json
+from pprint import pprint
 def obtenerTiempo(origen,destino):
 	try:
 	# Concatenamos la cadena con el url, para llamar la api de google. 
-		url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + Origen + "|&destinations=" + Destino + "&mode=driving&language=fr-SP"
+		url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + origen + "|&destinations=" + destino + "&mode=driving&language=fr-SP"
 		# url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=19.3022924,-99.11184889999998|&destinations=paseoacoxpa&mode=driving&language=fr-SP"
 		response = urllib.urlopen(url)
 		# Obtenemos el json
-		data = json.loads(response.read())
+		print("Logramos conectarnos con la API")
+		data = json.load(response)
+		print("Obtuvimos los datos...")
 
-		archivo = open("ElYeison.txt","w")
-		archivo2 = open("Respuesta.json","w")
+		# archivo = open("ElYeison.txt","w")
+		# archivo2 = open("Respuesta.json","w")
 		
-		archivo.write(data)
-		archivo.close()
-		archivo2.close()
-		print data
+		# archivo.write(data)
+		# archivo.close()
+		# archivo2.close()
+		print data['rows'][0]['elements'][0]['duration']['text']
 	except:
 		print("No se puede conectar a internet")
 	finally:
